2c16f8b61f482bc323b59b9ed24858d741b88ce7
mpaydar <m21newyork@gmail.com>
2022-06-08 19:02:44 -0400

    did bullet number 1,2,3

--- HW1/dataProcess.py
+++ HW1/dataProcess.py
@@ -2,5 +2,38 @@ import pandas as pd
 
 # reading the csv
 headers=["Price"," Maintenancecost","#ofdoors","#ofpassengers","Luggagecap",'Safetyrating',"VehicleClass"]
-df=pd.read_csv('https://raw.githubusercontent.com/mpaydar/CS381/main/HW1/cars-sample35.csv',names=headers,sep=',')
-print(df)
+df=pd.read_csv("https://raw.githubusercontent.com/mpaydar/CS381/main/HW1/cars-sample35.csv",names=headers)
+price=[]
+maintenance=[]
+doors=[]
+passengers=[]
+luggage=[]
+safetyRating=[]
+vehicle=[]
+
+
+
+price=df['Price'].tolist()
+maintenance=df[' Maintenancecost'].tolist()
+doors=df['#ofdoors'].tolist()
+passengers=df['#ofpassengers'].tolist()
+luggage=df['Luggagecap'].tolist()
+safetyRating=df['Safetyrating'].tolist()
+vehicles=df['VehicleClass'].tolist()
+
+print(len(price))
+
+priceIndexes = [p for p in range(len(price)) if price[p]=='med']
+print(priceIndexes)
+
+
+
+
+
+# price =[print(p) for p in range(len(price)) if p=='med']
+# print(price)
+
+
+
+# maintenance=[m for m in maintenance ]
+# print(maintenance)
